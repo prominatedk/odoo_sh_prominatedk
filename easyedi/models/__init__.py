2d5ec6a0c2039832b3ccdbae08136ebc843103ab
Shankar <shiv.shankar@cerner.com>
2020-02-15 12:23:09 +0530

    deleted easyedi directory

--- easyedi/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import edi
-from . import editransfer
-from . import partner
-from . import saleorder
-from . import invoice
-from . import purchaseorder
-from . import picking
-from . import company
