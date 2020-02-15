25b02501a43ad56b0a5ee5e83fb099e4c506a89d
Shankar <shiv.shankar@cerner.com>
2020-02-15 11:49:02 +0530

    updated module easyedi

---
+++ easyedi/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from . import edi
+from . import editransfer
+from . import partner
+from . import saleorder
+from . import invoice
+from . import purchaseorder
+from . import picking
+from . import company
