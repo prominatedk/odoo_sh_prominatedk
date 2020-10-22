23fbd5aab8e877d7e717ab2715814e4f01655486
Kenneth Hansen <kenneth@vkdata.dk>
2020-10-22 16:23:33 +0200

    [FIX] update primecarog edi to version 1.1.1

--- odoo_edi_primecargo_wms/__manifest__.py
+++ odoo_edi_primecargo_wms/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Primecargo WMS",
     "summary": "Please create a short summary for your new module",
     "description": "Please create an in depth description of your new module",
-    "version": "1.0.1",
+    "version": "1.1.1",
     "category": "Customization",
     "author": "VK Data ApS",
     "website": "https://vkdata.dk",
