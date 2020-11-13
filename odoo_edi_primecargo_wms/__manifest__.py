5f93cb2361145d987ffca9ca05185974a0583c0f
Kenneth Hansen <kenneth@vkdata.dk>
2020-11-13 13:04:17 +0100

    [IMP] Update Primecargo integration to version 1.3.14

--- odoo_edi_primecargo_wms/__manifest__.py
+++ odoo_edi_primecargo_wms/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Primecargo WMS",
     "summary": "Please create a short summary for your new module",
     "description": "Please create an in depth description of your new module",
-    "version": "1.3.13",
+    "version": "1.3.14",
     "category": "Customization",
     "author": "VK Data ApS",
     "website": "https://vkdata.dk",
--- odoo_edi_primecargo_wms/models/edi_stock_picking_outgoing.py
+++ odoo_edi_primecargo_wms/models/edi_stock_picking_outgoing.py
@@ -41,7 +41,7 @@ class EdiStockPickingOutgoing(models.TransientModel):
             'recipient_country': document.partner_id.country_id.code,
             'recipient_email': document.partner_id.email,
             'recipient_phone': document.partner_id.phone or document.partner_id.mobile,
-            'recipient_contact_name': document.partner_id.name if len(document.partner_id.display_name) < 35 else '',
+            'recipient_contact_name': document.partner_id.name if document.partner_id.parent_id.id else '',
             'shipping_product_code': document.primecargo_shipping_product_id.code or document.company_id.primecargo_shipping_product_id.code,
             'customer_number': document.partner_id.ref or document.partner_id.id,
             'salesorderline_set': [self.prepare_document_lines(line) for line in document.move_ids_without_package]
