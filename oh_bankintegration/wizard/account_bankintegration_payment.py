ec2b779a42721d393be8998befa4bec7a0f03053
Kenneth Hansen <kenneth@khansen-it.dk>
2020-12-01 12:06:37 +0100

    [IMP] Update bankintegraton to version 2.8.3

--- oh_bankintegration/wizard/account_bankintegration_payment.py
+++ oh_bankintegration/wizard/account_bankintegration_payment.py
@@ -1,39 +1,41 @@
-# -*- coding: utf-8 -*-
-from odoo import models, api, _
+from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 
-
 class AccountBankintegrationPayment(models.TransientModel):
-    """
-    This wizard will make payment for selected invoices
-    """
+    _name = 'account.bankintegration.payment'
+    _description = 'Pay selected invoices with bankintegration'
 
-    _name = "account.bankintegration.payment"
-    _description = "Bankintegration paymnet for selected invoices"
+    def _default_invoice_ids(self):
+        active_ids = self.env.context.get('active_ids', [])
+        if not active_ids:
+            return False
+        else:
+            return self.env['account.invoice'].browse(active_ids)
 
-    @api.multi
-    def make_bankintegration_payment(self):
-        context = dict(self._context or {})
-        active_ids = context.get('active_ids', []) or []
-        customer_payment = False
-        invoice_ids = []
-        invoice_model = self.env['account.invoice']
-        for record in invoice_model.browse(active_ids):
-            if record.type == 'out_invoice':
-                customer_payment = True
-                break
-            elif record.payment_status in ['created', 'pending', 'accepted', 'success', 'rejected', 'failed', 'not_found']:
-                raise UserError(_("One of the selected invoice is already under payment processing/processed status."))
-                break
-            else:
-                invoice_ids.append(record.id)
-            if record.payment_status and record.payment_status not in ['open']:
-                raise UserError(_("Selected invoice(s) cannot be paid as they are not in 'Open' status."))
-            # Write code here to make automatic payment
-        if customer_payment:
-            raise UserError(_("Access Denied."))
+    invoice_ids = fields.Many2many('account.invoice', default=_default_invoice_ids, ondelete='cascade')
+
+    def action_pay_with_bankintegration(self):
+        self.ensure_one()
+        if any([record.payment_status in ['created', 'pending', 'accepted', 'success', 'rejected', 'failed', 'not_found'] for record in self.invoice_ids]):
+            raise UserError(_('One or more of the selected invoices are already under payment with bankintegration'))
         
-        if invoice_ids:
-            invoice_model.bankintegration_schedule_payment(invoice_ids)
+        if any([record.state != 'open' for record in self.invoice_ids]):
+            raise UserError(_('One or more of the selected invoices has not yet been validated'))
 
-        return {'type': 'ir.actions.act_window_close'}
+        if not all([record.payment_journal.id == self.invoice_ids[0].payment_journal.id for record in self.invoice_ids]):
+            # We have mixed journals
+            journals = []
+            # Fetch all journals
+            # NOTE: The double loop below might not necessarily be the best solution as it will have performance issues on larger datasets
+            for record in self.invoice_ids:
+                if not record.payment_journal in journals:
+                    journals.append(record.payment_journal)
+            for journal in journals:
+                invoices = self.invoice_ids.filtered(lambda x: x.payment_journal.id == journal.id)
+                invoices._pay_with_bankintegration(journal, is_scheduler=False)
+
+        else:
+            # All invoices have the same journal. Process the entire recordset at once
+            self.invoice_ids._pay_with_bankintegration(self.invoice_ids[0].payment_journal, is_scheduler=False)
+
+        return {}
