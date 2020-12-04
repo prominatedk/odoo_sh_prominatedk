e1a7103fd423df97f96cd2896b045666fd72b840
Kenneth Hansen <kenneth@khansen-it.dk>
2020-12-04 12:47:00 +0100

    [IMP] Update monthly sales report module

--- monthly_sales_report/wizard/monthly_sales_report.py
+++ monthly_sales_report/wizard/monthly_sales_report.py
@@ -3,6 +3,7 @@ import io
 from odoo import api, fields, models, _
 import xlsxwriter
 
+
 class MonthlySalesReport(models.TransientModel):
     _name = 'monthly.sales.report'
     _description = 'XLS and PDF Reports'
@@ -21,9 +22,8 @@ class MonthlySalesReport(models.TransientModel):
         table_acc = workbook.add_format({'font_size': 12, 'num_format': '#,#'})
 
         data = self.env['sale.order'].search([('date_order', '>=', self.date_from),
-                                              ('date_order', '<=', self.date_to)])
-        sorted_data = sorted(data, key=lambda x: x.date_order)
-
+                                              ('date_order', '<=', self.date_to),
+                                              ('state', '=', 'sale')], order="date_order")
 
         sheet = workbook.add_worksheet('Monthly Sales Report')
         sheet.set_column('A:AP', 25)
@@ -72,127 +72,75 @@ class MonthlySalesReport(models.TransientModel):
 
         j = 1
 
-        for item in sorted_data:
-            orderlines = item.order_line
-            products = []
-            product_codes = []
-            product_prices = []
-            product_quantities = []
-            product_totals = []
-            product_categories = []
-            parent_categories = []
-            commodity_codes = []
-            project_codes = []
-            coos = []
-            for lines in orderlines:
-                products.append(lines.product_id.name)
-                product_codes.append(str(lines.product_id.default_code))
-                product_prices.append(lines.price_unit)
-                product_quantities.append(lines.product_uom_qty)
-                product_totals.append(lines.price_total)
-                product_categories.append(str(lines.product_id.categ_id.name))
-                parent_categories.append(str(lines.product_id.categ_id.parent_id.name))
-                commodity_codes.append(str(lines.product_id.x_studio_field_cA6I2))
-                project_codes.append(str(lines.product_id.x_studio_field_vdINR))
-                coos.append(str(lines.product_id.x_studio_field_hqRIw))
-            sheet.write(j, 0, '', table_body)
-            sheet.write(j, 1, item.name, table_body)
-            k = j
-            for project_code in project_codes:
-                sheet.write(k, 2, project_code, table_body)
-                k += 1
-            # sheet.write(j, 2, '', table_body)
-            sheet.write(j, 3, '', table_body)
-            sheet.write(j, 4, item.partner_id.name, table_body)
-            sheet.write(j, 5, '', table_body)
-            sheet.write(j, 6, '', table_body)
-            if item.partner_id.mobile:
-                sheet.write(j, 7, item.partner_id.mobile, table_body)
-            else:
-                sheet.write(j, 7, '', table_body)
-            if item.partner_id.street2:
-                sheet.write(j, 8, item.partner_id.street + ', ' + item.partner_id.street2, table_body)
-            else:
-                sheet.write(j, 8, item.partner_id.street, table_body)
-            sheet.write(j, 9, item.partner_id.zip, table_body)
-            sheet.write(j, 10, item.partner_id.city, table_body)
-            sheet.write(j, 11, item.partner_id.country_id.code, table_body)
-            sheet.write(j, 12, '', table_body)
-            sheet.write(j, 13, '', table_body)
-            sheet.write(j, 14, item.date_order, table_date)
-            sheet.write(j, 15, item.partner_id.country_id.code, table_body)
-            shipment = self.env['stock.picking'].search([('origin', '=', item.name)])
-            shipdates = []
-            for i in shipment:
-                shipdates.append(i.scheduled_date.strftime("%m/%d/%Y"))
-            shipdate = ",".join(shipdates)
-            sheet.write(j, 16, shipdate, table_body)
-            sheet.write(j, 17, item.currency_id.name, table_body)
-            sheet.write(j, 18, item.partner_shipping_id.name, table_body)
-            sheet.write(j, 19, '', table_body)
-            sheet.write(j, 20, item.partner_shipping_id.zip, table_body)
-            sheet.write(j, 21, item.partner_shipping_id.city, table_body)
-            sheet.write(j, 22, item.partner_shipping_id.country_id.code, table_body)
-            invoices = self.env['account.invoice'].search([('origin', '=', item.name)])
-            invoice_dates = []
-            invoice_amounts = []
-            for lines in invoices:
-                invoice_dates.append(str(lines.date_invoice))
-                invoice_amounts.append(str(lines.amount_total))
-            invoice_date = ",".join(invoice_dates)
-            invoice_amount = ",".join(invoice_amounts)
-            if len(invoice_dates) > 0:
-                sheet.write(j, 23, invoice_date, table_date)
-            else:
-                sheet.write(j, 23, '', table_date)
-            sheet.write(j, 24, invoice_amount, table_body)
-            sheet.write(j, 25, '', table_body)
-            k = j
-            for product_code in product_codes:
-                sheet.write(k, 26, product_code, table_body)
-                k += 1
-            k = j
-            for parent_category in parent_categories:
-                sheet.write(k, 27, parent_category, table_body)
-                k += 1
-            k = j
-            for product in products:
-                sheet.write(k, 28, product, table_body)
-                k += 1
-            k = j
-            for product_category in product_categories:
-                sheet.write(k, 29, product_category, table_body)
-                k += 1
-            k = j
-            for price in product_prices:
-                sheet.write(k, 30, price, table_acc)
-                k += 1
-            k = j
-            for quantity in product_quantities:
-                sheet.write(k, 31, quantity, table_body)
-                k += 1
-            k = j
-            for coo in coos:
-                sheet.write(k, 32, coo, table_body)
-                k += 1
-            k = j
-            for commodity_code in commodity_codes:
-                sheet.write(k, 33, commodity_code, table_body)
-                k += 1
-            k = j
-            for total in product_totals:
-                sheet.write(k, 34, total, table_acc)
-                k += 1
-            sheet.write(j, 35, '', table_body)
-            sheet.write(j, 36, '', table_body)
-            sheet.write(j, 37, '', table_body)
-            sheet.write(j, 38, '', table_body)
-            sheet.write(j, 39, '', table_body)
-            sheet.write(j, 40, '', table_body)
-            sheet.write(j, 41, '', table_body)
+        for item in data.mapped('order_line'):
+            if item.product_id.default_code not in ['1099', '1098', '1097', '1096', '1095']:
+                sheet.write(j, 0, '', table_body)
+                sheet.write(j, 1, item.order_id.name, table_body)
+                sheet.write(j, 2, item.product_id.x_studio_field_vdINR, table_body)
+                sheet.write(j, 3, '', table_body)
+                sheet.write(j, 4, item.order_id.partner_id.name + ' ' + item.order_id.partner_id.country_id.name,
+                            table_body)
+                sheet.write(j, 5, '', table_body)
+                sheet.write(j, 6, '', table_body)
+                if item.order_id.partner_id.mobile:
+                    sheet.write(j, 7, item.order_id.partner_id.mobile, table_body)
+                else:
+                    sheet.write(j, 7, '', table_body)
+                if item.order_id.partner_id.street2:
+                    sheet.write(j, 8, item.order_id.partner_id.street + ', ' + item.order_id.partner_id.street2, table_body)
+                else:
+                    sheet.write(j, 8, item.order_id.partner_id.street, table_body)
+                sheet.write(j, 9, item.order_id.partner_id.zip, table_body)
+                sheet.write(j, 10, item.order_id.partner_id.city, table_body)
+                sheet.write(j, 11, item.order_id.partner_id.country_id.code, table_body)
+                sheet.write(j, 12, '', table_body)
+                sheet.write(j, 13, '', table_body)
+                sheet.write(j, 14, item.order_id.date_order, table_date)
+                sheet.write(j, 15, item.order_id.partner_id.country_id.code, table_body)
+                shipment = self.env['stock.picking'].search([('origin', '=', item.order_id.name)])
+                shipdates = []
+                for i in shipment:
+                    shipdates.append(i.scheduled_date.strftime("%m/%d/%Y"))
+                shipdate = ",".join(shipdates)
+                sheet.write(j, 16, shipdate, table_body)
+                sheet.write(j, 17, item.order_id.currency_id.name, table_body)
+                sheet.write(j, 18, item.order_id.partner_shipping_id.name, table_body)
+                sheet.write(j, 19, '', table_body)
+                sheet.write(j, 20, item.order_id.partner_shipping_id.zip, table_body)
+                sheet.write(j, 21, item.order_id.partner_shipping_id.city, table_body)
+                sheet.write(j, 22, item.order_id.partner_shipping_id.country_id.code, table_body)
+                invoices = self.env['account.invoice'].search([('origin', '=', item.order_id.name)])
+                invoice_dates = []
+                invoice_amounts = []
+                for lines in invoices:
+                    invoice_dates.append(str(lines.date_invoice))
+                    invoice_amounts.append(str(lines.amount_total))
+                invoice_date = ",".join(invoice_dates)
+                invoice_amount = ",".join(invoice_amounts)
+                if len(invoice_dates) > 0:
+                    sheet.write(j, 23, invoice_date, table_date)
+                else:
+                    sheet.write(j, 23, '', table_date)
+                sheet.write(j, 24, invoice_amount, table_body)
+                sheet.write(j, 25, '', table_body)
+                sheet.write(j, 26, item.product_id.default_code, table_body)
+                sheet.write(j, 27, item.product_id.categ_id.parent_id.name, table_body)
+                sheet.write(j, 28, item.product_id.name, table_body)
+                sheet.write(j, 29, item.product_id.categ_id.name, table_body)
+                sheet.write(j, 30, item.price_unit, table_acc)
+                sheet.write(j, 31, item.product_uom_qty, table_body)
+                sheet.write(j, 32, item.product_id.x_studio_field_hqRIw, table_body)
+                sheet.write(j, 33, item.product_id.x_studio_field_cA6I2, table_body)
+                sheet.write(j, 34, item.price_total, table_acc)
+                sheet.write(j, 35, '', table_body)
+                sheet.write(j, 36, '', table_body)
+                sheet.write(j, 37, '', table_body)
+                sheet.write(j, 38, '', table_body)
+                sheet.write(j, 39, '', table_body)
+                sheet.write(j, 40, '', table_body)
+                sheet.write(j, 41, '', table_body)
 
-            if k >= j:
-                j = k
+                j += 1
 
         workbook.close()
 
