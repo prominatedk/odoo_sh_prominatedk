d3bdb5a1cb65352f34a701dcce64a57f51018c1e
Kenneth Hansen <kenneth@khansen-it.dk>
2020-12-17 09:17:27 +0100

    [FIX] Fix issues with stock updates for webshop in wrong unit

--- prominate_webshop_integration/__manifest__.py
+++ prominate_webshop_integration/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Prominate Webshop Integration",
     "summary": "Stock Syncronization",
     "description": "This module syncronizes the stock quantities between Odoo and the webshop",
-    "version": "1.9.9",
+    "version": "1.9.10",
     "category": "E-Commerce",
     "author": "VK Data ApS",
     "website": "https://vkdata.dk",
