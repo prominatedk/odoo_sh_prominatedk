0115a4d0afbfa5f196a017e5b473f5f9b522e193
Mads Soendergaard <mads@vkdata.dk>
2021-08-12 11:48:02 +0200

    [FIX] Removed credit_note as reliant field no longer exists (in a meaningful way)

--- monthly_sales_report/__manifest__.py
+++ monthly_sales_report/__manifest__.py
@@ -6,7 +6,7 @@
     'author': "Nisus Solutions (Pvt) Ltd",
     'website': "http://www.nisus.lk",
     'category': 'Sales',
-    'version': '1.0.7',
+    'version': '1.0.8',
     'depends': ['sale', 'account'],
     'data': [
         'security/ir.model.access.csv',
