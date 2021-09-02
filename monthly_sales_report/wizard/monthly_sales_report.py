0bc776045d162661237d11457ac1e88f22b78974
Mads Soendergaard <mads@vkdata.dk>
2021-09-02 08:29:47 +0200

    [FIX] Testing getting shipping dates for Dropship orders

--- monthly_sales_report/wizard/monthly_sales_report.py
+++ monthly_sales_report/wizard/monthly_sales_report.py
@@ -82,14 +82,14 @@ class MonthlySalesReport(models.TransientModel):
 
         for item in data.mapped('invoice_line_ids'):
             if item.product_id.default_code not in ['1099', '1098', '1097', '1096', '1095', '1093', '1099', '503', 'Environmental tax - Export'] and item.product_id:
-                sheet.write(j, 0, '', table_body)
-                sheet.write(j, 1, item.move_id.invoice_origin, table_body)
-                sheet.write(j, 2, item.product_id.x_studio_field_vdINR, table_body)
-                sheet.write(j, 3, '', table_body)
+                sheet.write(j, 0, '', table_body) #C
+                sheet.write(j, 1, item.move_id.invoice_origin, table_body) # B
+                sheet.write(j, 2, item.product_id.x_studio_field_vdINR, table_body) #C
+                sheet.write(j, 3, '', table_body) #D
                 project_string = str(item.product_id.x_studio_field_vdINR)
                 project_codes = project_string.split('_', 1)
                 project_code = project_codes[0]
-                if item.move_id.partner_id.parent_id and item.move_id.partner_id.parent_id.country_id.name:
+                if item.move_id.partner_id.parent_id and item.move_id.partner_id.parent_id.country_id.name: # E
                     sheet.write(j, 4, project_code + ' ' +
                                 str(item.move_id.partner_id.parent_id.country_id.name), table_body)
                 elif item.move_id.partner_id.parent_id:
@@ -99,65 +99,65 @@ class MonthlySalesReport(models.TransientModel):
                                 str(item.move_id.partner_id.country_id.name), table_body)
                 else:
                     sheet.write(j, 4, project_code, table_body)
-                sheet.write(j, 5, '', table_body)
-                sheet.write(j, 6, '', table_body)
+                sheet.write(j, 5, '', table_body) # F
+                sheet.write(j, 6, '', table_body) # G
                 if item.move_id.partner_id.mobile:
-                    sheet.write(j, 7, item.move_id.partner_id.mobile, table_body)
+                    sheet.write(j, 7, item.move_id.partner_id.mobile, table_body) # H
                 else:
-                    sheet.write(j, 7, '', table_body)
+                    sheet.write(j, 7, '', table_body) # H
                 if item.move_id.partner_id.street2:
-                    sheet.write(j, 8, item.move_id.partner_id.street + ', ' + item.move_id.partner_id.street2,
+                    sheet.write(j, 8, item.move_id.partner_id.street + ', ' + item.move_id.partner_id.street2, # I
                                 table_body)
                 else:
-                    sheet.write(j, 8, item.move_id.partner_id.street, table_body)
-                sheet.write(j, 9, item.move_id.partner_id.zip, table_body)
-                sheet.write(j, 10, item.move_id.partner_id.city, table_body)
-                sheet.write(j, 11, item.move_id.partner_id.country_id.code, table_body)
-                sheet.write(j, 12, '', table_body)
-                sheet.write(j, 13, '', table_body)
+                    sheet.write(j, 8, item.move_id.partner_id.street, table_body) # I
+                sheet.write(j, 9, item.move_id.partner_id.zip, table_body) # J
+                sheet.write(j, 10, item.move_id.partner_id.city, table_body) # K
+                sheet.write(j, 11, item.move_id.partner_id.country_id.code, table_body) # L
+                sheet.write(j, 12, '', table_body) # M
+                sheet.write(j, 13, '', table_body) # N
                 if item.mapped('sale_line_ids'):
-                    sheet.write(j, 14, item.mapped('sale_line_ids')[0].order_id.date_order.date(), table_date)
-                sheet.write(j, 15, item.move_id.partner_id.country_id.code, table_body)
+                    sheet.write(j, 14, item.mapped('sale_line_ids')[0].order_id.date_order.date(), table_date) # O
+                sheet.write(j, 15, item.move_id.partner_id.country_id.code, table_body) # P
                 if item.move_id.invoice_origin:
-                    shipment = self.env['stock.picking'].search([('origin', '=', item.move_id.invoice_origin)])
+                    shipment = self.env['stock.picking'].search([('sale_id.name', '=', item.move_id.invoice_origin)])
                     shipdates = []
                     for i in shipment:
                         if i.date_done:
                             shipdates.append(i.date_done.strftime("%d-%m-%Y"))
                     shipdate = ",".join(shipdates)
-                    sheet.write(j, 16, shipdate, table_body)
-                sheet.write(j, 17, item.move_id.currency_id.name, table_body)
-                sheet.write(j, 18, item.move_id.partner_shipping_id.name, table_body)
-                sheet.write(j, 19, '', table_body)
-                sheet.write(j, 20, item.move_id.partner_shipping_id.zip, table_body)
-                sheet.write(j, 21, item.move_id.partner_shipping_id.city, table_body)
-                sheet.write(j, 22, item.move_id.partner_shipping_id.country_id.code, table_body)
-                sheet.write(j, 23, item.move_id.invoice_date, table_date)
-                sheet.write(j, 24, item.price_subtotal, table_acc)
-                sheet.write(j, 25, '', table_body)
-                sheet.write(j, 26, item.product_id.default_code, table_body)
+                    sheet.write(j, 16, shipdate, table_body) # Q
+                sheet.write(j, 17, item.move_id.currency_id.name, table_body) # R
+                sheet.write(j, 18, item.move_id.partner_shipping_id.name, table_body) # S
+                sheet.write(j, 19, '', table_body) # T
+                sheet.write(j, 20, item.move_id.partner_shipping_id.zip, table_body) # U
+                sheet.write(j, 21, item.move_id.partner_shipping_id.city, table_body) # V
+                sheet.write(j, 22, item.move_id.partner_shipping_id.country_id.code, table_body) # W
+                sheet.write(j, 23, item.move_id.invoice_date, table_date) # X
+                sheet.write(j, 24, item.price_subtotal, table_acc) # Y
+                sheet.write(j, 25, '', table_body) # Z
+                sheet.write(j, 26, item.product_id.default_code, table_body) # AA
                 if item.product_id.categ_id.name:
                     try:
                         int(item.product_id.categ_id.name[:2])
                         sheet.write(j, 27, item.product_id.categ_id.name[3:], table_body)
                     except ValueError:
-                        sheet.write(j, 27, item.product_id.categ_id.name, table_body)
-                sheet.write(j, 28, item.product_id.name, table_body)
-                sheet.write(j, 29, item.product_id.x_studio_field_Mj7AZ, table_body)
-                sheet.write(j, 30, item.price_unit, table_acc)
-                sheet.write(j, 31, item.quantity, table_acc)
-                sheet.write(j, 32, item.product_id.x_studio_field_hqRIw, table_body)
-                sheet.write(j, 33, item.product_id.x_studio_field_cA6I2, table_body)
-                sheet.write(j, 34, item.price_total, table_acc)
-                sheet.write(j, 35, '', table_body)
-                sheet.write(j, 36, '', table_body)
-                if len(item.product_id.seller_ids) > 0:
+                        sheet.write(j, 27, item.product_id.categ_id.name, table_body) # AB
+                sheet.write(j, 28, item.product_id.name, table_body) # AC
+                sheet.write(j, 29, item.product_id.x_studio_field_Mj7AZ, table_body) # AD
+                sheet.write(j, 30, item.price_unit, table_acc) # AE
+                sheet.write(j, 31, item.quantity, table_acc) # AF
+                sheet.write(j, 32, item.product_id.x_studio_field_hqRIw, table_body) # AG
+                sheet.write(j, 33, item.product_id.x_studio_field_cA6I2, table_body) # AH
+                sheet.write(j, 34, item.price_total, table_acc) # AI
+                sheet.write(j, 35, '', table_body) # AJ
+                sheet.write(j, 36, '', table_body) # AK
+                if len(item.product_id.seller_ids) > 0: 
                     if item.product_id.seller_ids[0].name.audited:
-                        sheet.write(j, 37, 'Yes', table_body)
+                        sheet.write(j, 37, 'Yes', table_body) # AL
                     else:
-                        sheet.write(j, 37, 'No', table_body)
+                        sheet.write(j, 37, 'No', table_body) # AL
                 else:
-                    sheet.write(j, 37, 'No', table_body)
+                    sheet.write(j, 37, 'No', table_body) # AL
                 sheet.write(j, 38, '', table_body)
                 sheet.write(j, 39, '', table_body)
                 sheet.write(j, 40, '', table_body)
