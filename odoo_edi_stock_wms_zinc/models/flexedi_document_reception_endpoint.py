8263c0a6d43795f5c3f67ae80d7d0e8f0877a647
Kenneth Hansen <kenneth@vkdata.dk>
2022-01-24 16:12:43 +0100

    [FIX] Update zinc wms to 2.2.12

--- odoo_edi_stock_wms_zinc/models/flexedi_document_reception_endpoint.py
+++ odoo_edi_stock_wms_zinc/models/flexedi_document_reception_endpoint.py
@@ -301,7 +301,7 @@ class FlexediDocumentReceptionEndpoint(models.Model):
             'document_format_id': self.env.ref('odoo_edi_stock_wms_zinc.flexedi_document_format_zinc_wms_shipment_notice').id
         })
 
-        edi_sale_order = self.env['flexedi.document.wms.zinc.order'].search([('edi_id', '=', recieved_edi_document.origin_document_edi_id)], limit=1)
+        edi_sale_order = self.env['flexedi.document.wms.zinc.order'].search([('edi_id', '=', recieved_edi_document.origin_document_edi_id), ('document_format_id', '=', self.env.ref('odoo_edi_stock_wms_zinc.flexedi_document_format_zinc_wms_order').id)], limit=1)
         if not edi_sale_order.exists():
             recieved_edi_document.write({
                 'blocking_level': 'error',
