4ae239d951793cdba1c9544532f85eb51e59a4ad
Simon Andersen <simon@vkdata.dk>
2022-01-25 11:15:22 +0100

    [FIX] fix wrong data path in manifest file

--- country_vat_number/__manifest__.py
+++ country_vat_number/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': "Country Vat Number",
     'summary': 'Vat no. per country',
-    'version': '1.0.0',
+    'version': '1.0.1',
     'author': 'VK Data ApS',
     'category': 'Accounting',
     'description':'This module allows you to set a company vat no. for each country.',
@@ -9,9 +9,9 @@
     'depends': ['account'],
 
     'data': [
-        'account_tax_view.xml',
-        'external_layout_template.xml',
-        'report_invoice_template.xml',
-        'res_country_view.xml',
+        'views/account_tax_view.xml',
+        'views/external_layout_template.xml',
+        'views/report_invoice_template.xml',
+        'views/res_country_view.xml',
         ]
 }
