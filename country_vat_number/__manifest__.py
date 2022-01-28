2f82c706bda30310022c99fde5e975c82aaf1b35
Simon Andersen <simon@vkdata.dk>
2022-01-28 10:34:54 +0100

    [IMP] Add country specific vat no. to sale orders as well

--- country_vat_number/__manifest__.py
+++ country_vat_number/__manifest__.py
@@ -1,16 +1,17 @@
 {
     'name': "Country Vat Number",
     'summary': 'Vat no. per country',
-    'version': '1.0.4',
+    'version': '1.0.5',
     'author': 'VK Data ApS',
     'category': 'Accounting',
     'description':'This module allows you to set a company vat no. for each country.',
     'website': 'https://vkdata.dk',
-    'depends': ['account'],
+    'depends': ['account', 'sale'],
 
     'data': [
         'views/account_tax_view.xml',
         'views/external_layout_template.xml',
+        'views/report_saleorder_views.xml',
         'views/report_invoice_template.xml',
         'views/res_country_view.xml',
         ]
