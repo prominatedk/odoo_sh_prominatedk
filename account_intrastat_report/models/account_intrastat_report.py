31240c9c7feb0cded978d10502337ed7109ccc64
Mads Søndergaard <mads@vkdata.dk>
2022-04-12 13:25:56 +0200

    [IMP] Improved logic

--- account_intrastat_report/models/account_intrastat_report.py
+++ account_intrastat_report/models/account_intrastat_report.py
@@ -14,14 +14,28 @@ class IntrastatReport(models.AbstractModel):
 
     def _create_intrastat_report_line(self, options, vals):
         res = super(IntrastatReport, self)._create_intrastat_report_line(options, vals)
-        res += [{'name': c} for c in [
-            vals['fiscal_position']
-        ]]
+        # _logger.info(vals['fiscal_position'])
+        _logger.info(res['columns'][0])
+        res['columns'] += ([{'name': c} for c in [
+            vals['fiscal_position'],
+        ]])
+        _logger.info(res['columns'])
+        _logger.info("########################################")
+        # res += [{'name': c} for c in [
+        #     vals['fiscal_position'],
+        # ]]
         return res
 
     @api.model
     def _build_query(self, date_from, date_to, journal_ids, invoice_types=None, with_vat=False):
         res = super(IntrastatReport, self)._build_query(date_from, date_to, journal_ids, invoice_types=invoice_types, with_vat=with_vat)
-        res['select'] += '''
-            inv.fiscal_position_id as fiscal_position
+   
+        res[0]['select'] = '''
+            fisc.name as fiscal_position,
+        ''' + res[0]['select']
+
+        res[0]['from'] += '''
+            LEFT JOIN account_fiscal_position fisc ON inv.fiscal_position_id = fisc.id
         '''
+
+        return res
