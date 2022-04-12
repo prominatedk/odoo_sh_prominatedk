166e8b2963fa716fe75e8351dc3a15d3a313df8f
Mads Søndergaard <mads@vkdata.dk>
2022-04-12 16:24:12 +0200

    [NEW] Reconfiguration of EC Sales report to include fiscal position

---
+++ account_sales_report_fiscal/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Account EC Sales Report Fiscal Position",
+    "summary": "Adds Fiscal Position to EC sales report",
+    "description": "Adds Fiscal Position to EC sales report",
+    "version": "1.0.0",
+    "category": "Accounting",
+    "author": "VK Data ApS",
+    "website": "https://vkdata.dk",
+    "depends": [
+        'account', 'account_intrastat'
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "license": "OPL-1"
+}
